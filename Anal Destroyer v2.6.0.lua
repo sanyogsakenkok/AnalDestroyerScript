89f70fbc20200d70c0bd222f4227dd5b51abe83e
SanyogSakenKok <assqwesersed@gmail.com>
2025-06-08 00:47:56 +0300

    v2.6.0

--- Anal Destroyer v2.6.0.lua
+++ Anal Destroyer v2.6.0.lua
@@ -1,13 +1,7 @@
 local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
-task.spawn(function()
-    local PlayerGui = LocalPlayer:WaitForChild("PlayerGui")
-    local LoadScreen = PlayerGui:WaitForChild("LoadScreen")
-
-    LoadScreen.AncestryChanged:Connect(function(_, parent)
-        if not parent then -- LoadScreen был уничтожен
-            task.wait(1)
+task.spawn(function() local PlayerGui = LocalPlayer:WaitForChild("PlayerGui") local LoadScreen = PlayerGui:WaitForChild("LoadScreen") LoadScreen.AncestryChanged:Connect(function(_, parent) if not parent then task.wait(1)
             loadstring(game:HttpGet("https://raw.githubusercontent.com/sanyogsakenkok/AnalDestroyerScript/main/scripts/main.lua"))()
         end
     end)
