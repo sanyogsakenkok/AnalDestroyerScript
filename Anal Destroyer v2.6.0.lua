47efdf0c6dc44c4f5c3397a9324024d59527f0b4
SanyogSakenKok <assqwesersed@gmail.com>
2025-06-07 23:52:18 +0300

    v2.6.0

---
+++ Anal Destroyer v2.6.0.lua
@@ -0,0 +1,17 @@
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+task.spawn(function() local PlayerGui = LocalPlayer:WaitForChild("PlayerGui") local LoadScreen = PlayerGui:WaitForChild("LoadScreen") local LoadMenu = LoadScreen:WaitForChild("LoadMenu") local Navigation = LoadMenu:WaitForChild("Navigation") local StateLabel = Navigation:WaitForChild("State")
+    StateLabel:GetPropertyChangedSignal("Text"):Connect(function() if StateLabel.Text == "GAME IS STARTING!" then task.wait(3)
+        -- loading string here
+          
+        end
+    end)
+end)
+
+
+              -- Anal Destroyer Script
+              -- Created by: SanyokSuckenCock and GlebASS
+              -- Version: [ 2.6.0 ]
+
+              -- [ Last Changed ]
+              -- nothing rn...
